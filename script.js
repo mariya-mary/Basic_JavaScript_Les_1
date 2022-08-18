4e7e6a55dbae5bb61e19ef6e45bd92e424f73a35
Maria Turpakova <turpakovamaria@gmail.com>
2022-08-18 15:15:46 +0400

    total hw 1

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+// 1
+const Tc = +prompt('Ведите температуру в градусах по Цельсию:');
+const Tf = (9 % 5) * Tc + 32;
+alert(`${Tc}°С в градусах по Фаренгейту: ${Tf}°F`);
+
+// 2
+let admin;
+let name;
+name = 'Василий';
+admin = name;
+console.log(admin);
+
+// 3. *Чему будет равно JS-выражение 1000 + "108"
+//answer: 1000108
